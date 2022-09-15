0a116933c53710410997f0c100ca11dd37d3f403
LuckasCampos <112443731+LuckasCampos@users.noreply.github.com>
2022-09-15 15:39:42 -0300

    Finalização do metodo criarItem.
    Agora o método criarItem, tem o parâmetro da tarefa, que seria o que o usuário vai informar para ser inserido na lista.

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
 // <input type="button" value="X">
 // </label>
 
-const criarItem = () => {
+const criarItem = (tarefa) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
     <input type="checkbox">
-    <div>teste de item 1</div>
+    <div>${tarefa}</div>
     <input type="button" value="X">
     `
     document.getElementById('todoList').appendChild(item);
